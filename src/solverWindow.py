d8c117e12dcc464e4e33a62532c9d20d4dc17622
Duncan Caruthers <dcaruthe@uwyo.edu>
2022-01-10 17:46:52 -0700

    Added file upload capabilities to GUI

--- src/solverWindow.py
+++ src/solverWindow.py
@@ -1,5 +1,12 @@
 import tkinter as tk
-import tkinter.font as tkFont
+from tkinter import filedialog
+
+
+def uploadButtonFunction():
+    path = filedialog.askopenfilename()
+    uploadBox.delete(1.0, 'end')
+    uploadBox.insert(1.0, path)
+
 
 if __name__ == '__main__':
     # init window
@@ -10,12 +17,25 @@ if __name__ == '__main__':
 
     # Organization paneling
     leftPanel = tk.PanedWindow(window, orient=tk.HORIZONTAL,
-                               background='#595959', bg='#595959', height=700, width=250)
+                               background='#595959', bg='#595959', height=700, width=350)
     leftPanel.pack(side=tk.LEFT, fill=tk.BOTH)
 
     mainPanel = tk.PanedWindow(
-        window, orient=tk.HORIZONTAL, background='#808080', bg='#808080', height=700, width=750)
+        window, orient=tk.HORIZONTAL, background='#808080', bg='#808080', height=700, width=650)
     mainPanel.pack(side=tk.RIGHT, fill=tk.BOTH)
+
+    # file upload setup
+    uploadLabel = tk.Label(
+        leftPanel, text='Pieces Location:', background='#595959', fg='#cccccc')
+    uploadLabel.place(x=5, y=5, anchor=tk.NW)
+
+    global uploadBox
+    uploadBox = tk.Text(leftPanel, height=1, width=30)
+    uploadBox.place(x=100, y=5, anchor=tk.NW)
+
+    uploadButton = tk.Button(leftPanel, text='Open',
+                             command=uploadButtonFunction, height=1, width=10)
+    uploadButton.place(x=265, y=35, anchor=tk.NW)
 
     # Run
     window.mainloop()
