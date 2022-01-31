ec4dbf84b663290539de3293152a6fd172533fb4
Duncan Caruthers <dcaruthe@uwyo.edu>
2022-01-30 17:44:54 -0700

    Added file box stuff

--- src/solverWindow.py
+++ src/solverWindow.py
@@ -1,6 +1,7 @@
 import pygame
 import time
 import button
+import fileBox
 
 
 class Window:
@@ -64,6 +65,15 @@ class Window:
         button_unselected = (50, 50, 50)
         # set the buttons font
         f2 = pygame.font.Font(pygame.font.get_default_font(), 12)
+
+        # Run Button stuff
+        run_button = button.Button(
+            'Run', 10, 610, 230, 30, f2, off_white, button_selected, button_unselected)
+        run_button.draw(self.window)
+        if self.clicked and run_button.isInButton(self.last_click_x, self.last_click_y):
+            self.clicked = False
+            print('The run button was clicked!')
+
         # Create button object
         add = button.Button('+', 10, 60, 230, 30, f2,
                             off_white, button_selected, button_unselected)
@@ -74,16 +84,14 @@ class Window:
             self.clicked = False
             print('Add button was clicked!')
 
-        # Run Button stuff
-        run_button = button.Button(
-            'Run', 10, 610, 230, 30, f2, off_white, button_selected, button_unselected)
-        run_button.draw(self.window)
-        if self.clicked and run_button.isInButton(self.last_click_x, self.last_click_y):
-            self.clicked = False
-            print('The run button was clicked!')
+        # Temp file box
+        # file_null = fileBox.FileBox('NULL', 10, 100, 230, 30, f2)
+        # file_null.draw(self.window)
+        # if self.clicked and file_null.isInButton(self.last_click_x, self.last_click_y):
+        #     self.clicked = False
+        #     print('X pressed')
 
     # Render stuff to the window
-
     def render(self):
         # Clear screen with white
         self.window.fill((255, 255, 255))
