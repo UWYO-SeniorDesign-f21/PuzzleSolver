a13bf67648d75332f5f4d7df98b1a0c2f3f445ca
Jimmy <jwheelo1@uwyo.edu>
2022-04-20 18:58:58 -0600

    oops

--- src/puzzleSolution.py
+++ src/puzzleSolution.py
@@ -1557,21 +1557,25 @@ class PuzzleSolution:
                 desired_corner_poses = (corner_poses + np.array([pw/2, ph/2])).astype(np.float32)
 
                 
-                if not left_corners is None:
-                    # desired_corner_poses[0] = corner_poses[0]
-                    desired_corner_poses[corner_order[3]] = desired_corner_poses[corner_order[0]] + (left_corners[corner_order[2]] - left_corners[corner_order[1]])
+                if not (x == min_x and len(flat_edges) > 0 and rel_edge == 3):
+                    if not left_corners is None:
+                        # desired_corner_poses[0] = corner_poses[0]
+                        desired_corner_poses[corner_order[3]] = desired_corner_poses[corner_order[0]] + (left_corners[corner_order[2]] - left_corners[corner_order[1]])
 
                 if not (y == min_y and len(flat_edges) > 0 and rel_edge == 0):
                     if not above_corners is None:
                         desired_corner_poses[corner_order[1]] = desired_corner_poses[corner_order[0]] + (above_corners[corner_order[2]] - above_corners[corner_order[3]])
-                elif x == min_x and len(flat_edges) == 2:
+                
+
+                if len(flat_edges) == 2:
                     desired_corner_poses[corner_order[1]] = np.array([desired_corner_poses[corner_order[1]][0], desired_corner_poses[corner_order[0]][1]])
-                if x == max_x and rel_edge == 1:
-                    desired_corner_poses[corner_order[2]] = desired_corner_poses[corner_order[1]] + (corner_poses[corner_order[2]] - corner_poses[corner_order[1]])
-                elif y == max_y and rel_edge == 2:
-                    desired_corner_poses[corner_order[2]] = desired_corner_poses[corner_order[3]] + (corner_poses[corner_order[2]] - corner_poses[corner_order[3]])
-                elif x == max_x and y == max_y and len(flat_edges) == 2:
+                    desired_corner_poses[corner_order[3]] = np.array([desired_corner_poses[corner_order[0]][0], desired_corner_poses[corner_order[3]][1]])
                     desired_corner_poses[corner_order[2]] = np.array([desired_corner_poses[corner_order[1]][0], desired_corner_poses[corner_order[3]][1]])
+                elif len(flat_edges) == 1:
+                    if x == max_x and rel_edge == 1:
+                        desired_corner_poses[corner_order[2]] = desired_corner_poses[corner_order[1]] + (corner_poses[corner_order[2]] - corner_poses[corner_order[1]])
+                    elif y == max_y and rel_edge == 2:
+                        desired_corner_poses[corner_order[2]] = desired_corner_poses[corner_order[3]] + (corner_poses[corner_order[2]] - corner_poses[corner_order[3]])
                 else:
                     desired_corner_poses[corner_order[2]] = corner_poses[corner_order[2]] + (desired_corner_poses[corner_order[1]] - corner_poses[corner_order[3]] + desired_corner_poses[corner_order[3]] - corner_poses[corner_order[1]]) / 2
                 
